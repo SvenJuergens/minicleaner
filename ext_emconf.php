d862d5ff0266ebd7f7d72c1b47cf66cb79bbb5fe
Sven Jürgens <sj@nordsonne.de>
2015-08-31 22:36:00 +0200

    TER version 1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,17 +14,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'MiniCleaner',
 	'description' => 'Scheduler Task for deleting files in configurable folders',
 	'category' => 'be',
-	'author' => 'Sven Jürgens',
+	'author' => 'Sven Juergens',
 	'author_email' => 't3@blue-side.de',
 	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.3.99',
+			'typo3' => '6.2.0-7.4.99',
 		),
 		'conflicts' => array(
 		),
