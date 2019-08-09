dcb18d4b179bc3884fd70e70066892011e3c9676
SvenJuergens <sj@nordsonne.de>
2019-08-09 15:29:15 +0200

    [TASK] Set minimum TYPO3 Verison to 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.13-9.5.99',
+            'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [
         ],
