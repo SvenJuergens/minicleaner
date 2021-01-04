bcadca72a7b9b1cda5b625832963153d9551ead5
SvenJuergens <SvenJuergens@users.noreply.github.com>
2021-01-04 21:53:48 +0100

    [TASK] Added compatibility for TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.2.0',
+    'version' => '2.3.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.20-10.4.99',
+            'typo3' => '9.5.24-11.9.99',
+            'scheduler' => '9.5.24-11.9.999',
         ],
         'conflicts' => [
         ],
